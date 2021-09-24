b62e5242fbd9c8155f84d16baf34ddb03a017521
Hael12122002 <hendralijaya@gmail.com>
2021-09-24 11:37:37 +0700

    Menambahkan authRoutes post

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectMongoDB = require("./databases/mongoDBCon");
+const authRoutes = require("./routes/authRoutes");
 
 dotenv.config();
 connectMongoDB();
@@ -12,5 +13,6 @@ app.use(express.json()); //parsing json
 app.use(express.urlencoded({ extended: false }));
 
 // Routes
+app.use(authRoutes);
 
 app.listen(process.env.PORT, () => console.log(`Backend server is listening`));
--- controllers/authController.js
+++ controllers/authController.js
@@ -9,3 +9,21 @@ module.exports.register_post = async (req, res) => {
     console.log(err);
   }
 };
+
+module.exports.login_post = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const account = await Account.findOne({ email });
+    if (account) {
+      if (password === account.password) {
+        res.status(200).json({ account: account.email, msg: "Login successfully" });
+      } else {
+        res.status(400).json({ msg: "Wrong password" });
+      }
+    } else {
+      res.status(400).json({ msg: "Wrong email" });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
